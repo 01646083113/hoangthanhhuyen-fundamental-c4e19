6fb7c6532fbb2cc53fb7de1587a1e7e9009378f4
huyen <linhchipno98@gmail.com>
2018-07-13 11:45:37 +0700

    initial commit

--- Session05/homeword/Ex_3.py
+++ Session05/homeword/Ex_3.py
@@ -1,6 +1,4 @@
 bacterias = int(input("=====> How many B bacterias are there? "))
 time = int(input("=====> How much time in minutes will we wait? "))
-phase = int(input("=====> Enter the number phase : "))
-phase = time // 2
-total = bacterias * 2 ** (phase)
+total = bacterias * 2 ** (time // 2)
 print ("After {0} minutes(s) we would have {1} B Bacterias".format(time, total))
